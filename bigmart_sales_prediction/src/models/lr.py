3e25ce7d0c1b8679f2d6a638b0587c184a145f9a
waheed Ur Rahman <waheedkhan957759@gmail.com>
2025-07-06 18:00:20 +0500

    initial commit

--- bigmart_sales_prediction/src/models/lr.py
+++ bigmart_sales_prediction/src/models/lr.py
@@ -1,1 +1,46 @@
-# evaluate_model.py
+import pandas as pd
+from sklearn.linear_model import LinearRegression
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import mean_squared_error, r2_score
+import os
+import pickle
+
+def load_dataset(filepath="data\processed\train.csv"):
+    ''' Load dataset and also return it as a pandas DataFrame'''
+    
+    return pd.read_csv(filepath)
+
+
+def separat_features_target(df, target_column):
+    '''sepearet features and target columns from the dataset'''
+
+    X = df.drop(columns=[target_column])
+    y = df[target_column]
+    return X,y
+
+
+def split_data(X,y, test_size=0.2, random_state=42):
+    '''Split the dataset into training and testing sets'''
+    
+    return train_test_split(X, y, test_size=0.2, randome_state=42)
+
+
+def train_model(X_train, y_train):
+    '''Train the model'''
+    model = LinearRegression()
+    y_pred = model.fit(X_train, y_train)
+    return model
+
+def Evaluate_model():
+    
+
+def save_model():
+    pass
+
+def main():
+    pass
+
+
+
+
+
---
+++ bigmart_sales_prediction/src/pipeline/trainig_pipeline.py
@@ -0,0 +1,30 @@
+from src.models.lr import main
+
+processed_columns = [
+    'Item_Weight',
+    'Item_Visibility',
+    'Item_MRP',
+    'Outlet_Establishment_Year',
+    'Item_Identifier_encoded',
+ 'Item_Type_encoded',
+ 'Item_Fat_Content_encoded_LF',
+ 'Item_Fat_Content_encoded_REG',
+ 'Outlet_Location_Type_Tier 1',
+ 'Outlet_Location_Type_Tier 2',
+ 'Outlet_Location_Type_Tier 3',
+ 'Outlet_Size_High',
+ 'Outlet_Size_Medium',
+ 'Outlet_Size_Small',
+ 'Outlet_Type_Grocery Store',
+ 'Outlet_Type_Supermarket Type1',
+ 'Outlet_Type_Supermarket Type2',
+ 'Outlet_Type_Supermarket Type3',
+ 'Sales'
+]
+
+if __name__ == "__main__":
+    # Example usage
+    file_path = "data\processed\train.csv"  # Replace with your dataset path
+    
+    target_column = "Sales"  # Replace with your target column name
+    main(file_path, target_column, processed_columns)
